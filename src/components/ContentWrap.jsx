3699b3a431e90859374d58a7d3a82a13bcb3ffa6
Marta <mabutti@gmail.com>
2024-03-23 21:32:37 -0300

    first push to repository

---
+++ src/components/ContentWrap.jsx
@@ -0,0 +1,54 @@
+import { Route, Routes } from "react-router-dom";
+
+import GenresInDb from "./Genres/GenresInDb";
+import Error404 from "./Error404";
+import GenreDetail from "./Genres/GenreDetail";
+import Movies from "./Movies/Movies";
+import MasPopulares from "./MasPopulares/MasPopulares";
+import Statistics from "./Statistics/Statistics";
+import PreSales from "./PreSales/PreSales";
+
+export default function ContentWrap() {
+  return (
+    <>
+      <main className="content-wrap">
+        <Routes>
+          <Route path="/" exact element={<PreSales />} />
+          <Route path="/genres" exact element={<GenresInDb />} />
+          <Route path="/genres/:id" element={<GenreDetail />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/maspopulares" element={<MasPopulares />} />
+          <Route
+            path="/statistics"
+            element={
+              <Statistics
+                props={[
+                  {
+                    titulo: "Cantidad de estrenos",
+                    cifra: 15,
+                    icono: "bi bi-film",
+                    colorIcono: "cornflowerblue",
+                  },
+                  {
+                    titulo: "Categorias",
+                    cifra: 12,
+                    icono: "bi bi-tags-fill",
+                    colorIcono: "orange",
+                  },
+                  {
+                    titulo: "Total ventas",
+                    cifra: 489.567,
+                    icono: "bi bi-currency-dollar",
+                    colorIcono: "green",
+                  },
+                ]}
+              />
+            }
+          />
+          <Route component={Error404} />
+        </Routes>
+
+      </main>
+    </>
+  );
+}
---
+++ src/components/MenuWrap.jsx
@@ -0,0 +1,65 @@
+import logo from '../assets/img/logo.png'
+import { Link } from 'react-router-dom'
+
+export default function MenuWrap() {
+
+    return( 
+        <header className="menu-wrap">
+        <figure className="user">
+            <div className="user-avatar">
+            <Link to="/movies"> 
+                <img src={logo} alt="Logo Huellitas"/>
+            </Link>            
+            </div>
+            <figcaption>
+                Huellitasantiago
+            </figcaption>
+            
+        </figure>
+        <nav>
+            <section className="dicover">
+                <h3>Opciones</h3>
+                <ul>
+                <li>
+                    <Link to="/">
+                            <i className="bi bi-house" style={{fontSize: "1.2rem", color: "cornflowerblue"}}></i>
+                            - Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/movies">
+                            <i className="bi bi-film" style={{fontSize: "1.2rem", color: "cornflowerblue"}}></i>
+                            - Películas
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/genres">
+                            <i className="bi bi-person" style={{fontSize: "1.2rem", color: "cornflowerblue"}}></i>
+                            - Géneros
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/maspopulares">
+                            <i className="bi bi-bar-chart"></i>
+                            - Más populares
+                        </Link>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="bi bi-tags"></i>
+                            - Promociones
+                        </a>
+                    </li>
+                    <li>
+                        <Link to="/statistics">
+                            <i className="bi bi-graph-up"></i>
+                            - Estadísticas
+                        </Link>
+                    </li>
+                </ul>
+            </section>
+        </nav>
+    </header>
+    )
+    
+}
