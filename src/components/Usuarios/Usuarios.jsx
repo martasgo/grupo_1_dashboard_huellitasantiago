4a4543ca70a48480af9b0ef4889ebb337150e0a0
diegoa1501 <diegoa1501@gmail.com>
2024-03-29 00:41:36 -0300

    users pagination

--- src/components/Usuarios/Usuarios.jsx
+++ src/components/Usuarios/Usuarios.jsx
@@ -4,15 +4,26 @@ import style from "./Usuarios.module.css";
 
 function Usuarios(){
 const [usuarios, setUsuarios] = useState([]);
+const [pagina, setPagina] = useState(1);
 
   useEffect(()=> {
-    fetch("http://localhost:3000/api/users")
+    fetch(`http://localhost:3000/api/users?page=${pagina}&pageUsers=2`)
       .then((respuesta) => respuesta.json())
-      .then((result) => 
-      setUsuarios(result.users)
-      );
-  },[]
-)
+      .then((result) => setUsuarios(result.users))
+      .catch((error) => console.error(error));
+  },[pagina]);
+
+  const previousPage = () => {
+    if (pagina > 1) {
+      setPagina(pagina - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (usuarios.length === 2) {
+      setPagina(pagina + 1);
+    }
+  };
     
 return (
     <section className="content">
@@ -21,6 +32,16 @@ return (
         {usuarios.map((user) => (
             <ListadoUsuarios usuario={user} key={user.id} />
         ))}
+
+        <br />
+        {pagina > 1 && (
+          <button onClick={previousPage}>Página Anterior</button>
+        )}
+        <br />
+        <br />
+        {usuarios.length === 2 && (
+          <button onClick={nextPage}>Página Siguiente</button>
+        )}
 
     </section>
   )
