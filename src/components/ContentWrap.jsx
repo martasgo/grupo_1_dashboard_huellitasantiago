21b8033b346d5560a7d1bbef9c7216b21c1012c3
SoleSmith <fernanda.smith.ss@gmail.com>
2024-03-30 23:48:51 -0300

    clean code and order routes

--- src/components/ContentWrap.jsx
+++ src/components/ContentWrap.jsx
@@ -1,27 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 
-import Error404 from "./Error404";
-import Movies from "./Movies/Movies";
-import MasPopulares from "./MasPopulares/MasPopulares";
-import Statistics from "./Statistics/Statistics";
-import PreSales from "./PreSales/PreSales";
+import Home from "./Home/Home";
 import Productos from "./Productos/Productos";
 import DetalleProducto from "./Productos/DetalleProducto";
 import LastProduct from "./Productos/LastProduct";
 import Clientes from "./Usuarios/Usuarios";
+import Statistics from "./Statistics/Statistics";
+import Error404 from "./Error404";
 
 export default function ContentWrap() {
   return (
     <>
       <main className="content-wrap">
         <Routes>
-          <Route path="/" exact element={<PreSales />} />
+          <Route path="/" exact element={<Home />} />
           <Route path="/productos" exact element={<Productos />} />
           <Route path="/productos/:idProd" exact element={<DetalleProducto />} />
           <Route path="/lastproduct" exact element={<LastProduct />} />
-          <Route path="/usuarios" exact element={<Clientes />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/maspopulares" element={<MasPopulares />} />
+          <Route path="/usuarios" exact element={<Clientes />} />          
           <Route
             path="/statistics"
             element={
@@ -30,7 +26,6 @@ export default function ContentWrap() {
           />
           <Route component={Error404} />
         </Routes>
-
       </main>
     </>
   );
